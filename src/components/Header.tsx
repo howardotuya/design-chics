431ffa260834612ee3be6f2b22b7f2559ec91fbe
lamilekan263 <lamilekan263@gmail.com>
2024-06-05 12:29:10 +0100

    testing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,14 @@
 import React from 'react'
 
 import MenuLg from './menu/MenuLg'
-// import MenuSm from './menu/MenuSm'
+import MenuSm from './menu/MenuSm'
 
 const Header = () => {
     return (
         <div className='container my-10 font-roc-grotesk font-thin'>
             <>
                 <MenuLg />
-                {/* <MenuSm /> */}
+                <MenuSm />
             </>
         </div>
     )
